c0108f6b5824c47467ce194a9d5053ec51e8ff02
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-27 13:30:57 +0300

    imported all the necessary сomponents to the root component App

--- src/main.js
+++ src/main.js
@@ -3,14 +3,33 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import store from './store';
+import './plugins/plugins'
+
+import AppStartRate from "./components/StartRate"
+import AppTimer from "./components/Timer"
+import AppStatusInfoLabel from "./components/StatusInfoLabel"
+import AppIncreasingAndApproval from "./components/IncreasingAndApproval"
+import AppListOfRoundsActive from "./components/ListOfRoundsActive"
+import AppListOfRoundsCompleted from "./components/ListOfRoundsCompleted"
+import AppStatusTimerLine from "./components/StatusTimerLine"
+import AppHongAudioTrack from "./components/HongAudioTrack"
+
+Vue.component('AppStartRate', AppStartRate)
+Vue.component('AppTimer', AppTimer)
+Vue.component('AppStatusInfoLabel', AppStatusInfoLabel)
+Vue.component('AppListOfRoundsActive', AppListOfRoundsActive)
+Vue.component('AppListOfRoundsCompleted', AppListOfRoundsCompleted)
+Vue.component('AppIncreasingAndApproval', AppIncreasingAndApproval)
+Vue.component('AppStatusTimerLine', AppStatusTimerLine)
+Vue.component('AppHongAudioTrack', AppHongAudioTrack)
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
-});
 
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
