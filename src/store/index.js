561d4a53a3fa02dc78509704bbce83024c1e829b
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-27 13:18:53 +0300

    transferred src/store/store.js to src/store/index.js

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    apiUrl: 'https://jsonplaceholder.typicode.com/todos',
+    userInfo: {
+      todos: [],
+    },
+  },
+  mutations: {
+    setUserInfo(state, data) {
+      state.userInfo.todos = data;
+    },
+  },
+  actions: {
+    getUserInfo(context) {
+ 
+     axios.get(context.state.apiUrl)
+        .then((response) => {
+          context.commit('setUserInfo', response);
+        })
+        .catch((error) => {
+          console.log(error.response.statusText);
+       });
+    },
+  },
+});
