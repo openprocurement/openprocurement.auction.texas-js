0986e45735e202abab2db239352bdfb1b1d97971
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-27 13:25:45 +0300

    createde pluging folder with plugins.js file with imported several packages-plugins for projects

---
+++ src/plugins/plugins.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+import VueMoment from 'vue-moment'
+import VueChatScroll from 'vue-chat-scroll'
+
+import VueScrollTo from 'vue-scrollto';
+
+Vue.use(VueScrollTo)
+Vue.use(VueMoment);
+Vue.use(BootstrapVue);
+Vue.use(VueChatScroll);
+Vue.use(Vuetify, {
+})
