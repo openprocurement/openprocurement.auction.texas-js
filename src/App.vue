5d79aba616be1134fb9e71a856833e093aad5bcc
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-30 10:06:02 +0300

    modified App.vue, added new router-links

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <router-link :to='"auctions/active"'>active</router-link>
-    <router-link :to='"auctions/completed"'>completed</router-link>
+    <router-link :to='"auctions/active"'>active</router-link><br/>
+    <router-link :to='"auctions/completed"'>completed</router-link><br/>
+    <router-link :to='"auctions/pendingOfAuction"'>pendingOfAuction</router-link><br/>
+    <router-link :to='"auctions/pendingOfRound"'>pendingOfRound</router-link><br/>
       <router-view/>
   </div>
 </template>
