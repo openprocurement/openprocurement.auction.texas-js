a96942adc5aa1e21237a8163b46f2df3914c6ee2
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-30 10:00:01 +0300

    added component with checkbox toggle for choosing languages in modal window

---
+++ src/components/LanguagesToggleSwitch.vue
@@ -0,0 +1,54 @@
+<template>
+<toggle-switch
+  :options="myOptions"
+  @change="updateMap($event.value)" 
+  @selected="selectedMethod()" 
+  v-model="selectedMapOption"
+  :value="selectedMapOption" 
+  :group="switchGroup"
+  /> 
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            myOptions: {
+  layout: {
+    color: 'black',
+    backgroundColor: '#fff',
+    selectedColor: 'white',
+    selectedBackgroundColor: 'green',
+    borderColor: 'lightgrey',
+    fontFamily: 'Arial',
+    fontWeight: 'normal',
+    squareCorners: false,
+    noBorder: false
+  },
+  size: {
+    fontSize: 14,
+    height: 34,
+    padding: 7,
+    width: 250
+  },
+  items: {
+    delay: .4,
+    preSelected: 'unknown',
+    disabled: false,
+    labels: [
+      {name: 'English', color: '#000', backgroundColor: 'lightgrey'}, 
+      {name: 'Українська', color: '#000', backgroundColor: 'lightgrey'},
+      {name: 'Русский', color: '#000', backgroundColor: 'lightgrey'}
+    ]
+  }
+}
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
+
