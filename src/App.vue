6cf8c118a6c608e90e5e3393019539e3842deff1
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-31 16:26:23 +0300

    changed all separate rounter by one file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
-    <router-link :to='"auctions/active"'>active</router-link><br/>
-    <router-link :to='"auctions/completed"'>completed</router-link><br/>
-    <router-link :to='"auctions/pendingOfAuction"'>pendingOfAuction</router-link><br/>
-    <router-link :to='"auctions/pendingOfRound"'>pendingOfRound</router-link><br/>
       <router-view/>
-  </div>
+      </div>
 </template>
 
 <style>
