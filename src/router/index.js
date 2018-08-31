55b0ec60e7981c274aa7101ffe66ad02834e6e79
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-31 16:23:04 +0300

    added new path / ti router root file

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import MainView from '../views/MainView';
+import RouterStructure from '../views/RouterStructure';
 
 Vue.use(Router);
 export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/auctions/:id',
+      path: '/:id',
       name: 'auctionState',
       component: MainView,
+      props: true,
+    },
+
+    {
+      path: '/',
+      name: 'routerStructure',
+      component: RouterStructure,
       props: true,
     },
   ],
