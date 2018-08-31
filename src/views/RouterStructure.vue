9bfc305b00eb73f2a6e0e91887bca25506f16524
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-08-31 16:19:08 +0300

    added file with router-structure of app

---
+++ src/views/RouterStructure.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+    <router-link :to='"/active"'>active</router-link><br/>
+    <router-link :to='"/completed"'>completed</router-link><br/>
+    <router-link :to='"/pendingOfAuction"'>pendingOfAuction</router-link><br/>
+    <router-link :to='"/pendingOfRound"'>pendingOfRound</router-link><br/>
+    </div>
+</template>
