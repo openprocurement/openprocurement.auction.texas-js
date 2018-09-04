2cb737a7bfc8a6216868be90b7afe0575d55e9ef
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-04 18:03:37 +0300

    deleted redundant plugin from root src/plugins/plugins.js

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -4,8 +4,7 @@ import 'vuetify/dist/vuetify.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import BootstrapVue from 'bootstrap-vue';
-import VueMoment from 'vue-moment';
-import VueChatScroll from 'vue-chat-scroll';
+import VueMoment from 'vue-moment';;
 import VueScrollTo from 'vue-scrollto';
 import ToggleSwitch from 'vuejs-toggle-switch';
 
@@ -13,6 +12,5 @@ Vue.use(ToggleSwitch);
 Vue.use(VueScrollTo);
 Vue.use(VueMoment);
 Vue.use(BootstrapVue);
-Vue.use(VueChatScroll);
 Vue.use(Vuetify, {
 });
