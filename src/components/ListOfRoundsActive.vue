606d75d3da6c0a5f0fa7bc90aba7658bd948d918
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-06 17:43:47 +0300

    deleted redundatn updated lifeycle hook

--- src/components/ListOfRoundsActive.vue
+++ src/components/ListOfRoundsActive.vue
@@ -90,9 +90,6 @@ data () {
         this.value = (100 - (this.remainedTimeOfRound / this.durationOfRound * 100)).toFixed(2);
     },
   },
-//    updated() {
-//      window.scrollTo(0, document.body.scrollHeight);
-//   },
   components: {
       RadialProgressBar,
       AppListInitialOffers
