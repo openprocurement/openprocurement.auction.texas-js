fb0c7feeb55743525f60d28164eb86ea43d66c06
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-13 16:09:38 +0300

    added defined  urls for couch and pouch

--- src/utils/CouchPouch.js
+++ src/utils/CouchPouch.js
@@ -1,14 +1,15 @@
 import PouchDB from 'pouchdb'
 import infoPouch from '@/fakeInfo/infoPouchCouch'
 import store from '../store';
+import config from '../config.json'
 
-const db = new PouchDB('auction');
-let remoteDB = new PouchDB('http://localhost:5984/c1')
+const db = new PouchDB(config.pouchURL);
+let remoteDB = new PouchDB(config.couchURL)
 const fetch = () => {
   db.allDocs({include_docs: true})
     .then(r => r.rows)
     .then(data => {
-      store.commit('setInfoFromCouch', infoPouch);
+        store.commit('setInfoFromCouch', infoPouch);
     })
     .catch(console.error)
 }
