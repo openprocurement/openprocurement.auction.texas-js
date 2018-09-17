e085665af67dba4b3d006aace58d47e0f46f2b03
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-17 19:36:06 +0300

    added store as argument to function

--- src/utils/CouchPouch.js
+++ src/utils/CouchPouch.js
@@ -1,15 +1,15 @@
 import PouchDB from 'pouchdb'
 import infoPouch from '@/fakeInfo/infoPouchCouch'
-import store from '../store';
 import config from '../config.json'
 
 const db = new PouchDB(config.pouchURL);
 let remoteDB = new PouchDB(config.couchURL)
-const fetch = () => {
+const fetch = (store) => {
+  store.commit('setInfoFromCouch', 'infoPouch');
   db.allDocs({include_docs: true})
     .then(r => r.rows)
     .then(data => {
-      store.commit('setInfoFromCouch', infoPouch);
+      store.commit('setInfoFromCouch', 'infoPouch');
     })
     .catch(console.error)
 }
