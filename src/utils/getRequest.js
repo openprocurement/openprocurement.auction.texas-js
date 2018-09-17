45e833659ffcf0b85629f489402a9bc90d7b958b
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-17 19:30:47 +0300

    added file with getRequest and passing data to MainView component

---
+++ src/utils/getRequest.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+const getAuctionRequest =  (context, id) =>{
+  axios.get(`${context.$store.state.urls.auctionURL}database/${id}`)
+    .then(response => {
+      context.initialBidsArr = response.data.initial_bids
+      context.startBid = response.data.initial_value
+      context.current_stage = response.data.current_stage
+      context.current_type = response.data.stages[response.data.stages.length - 1].type
+      context.endDate = response.data.endDate
+      context.stages = response.data.stages
+      context.companyName = response.data.procuringEntity.name
+      context.descriptionOfProducts = response.data.description
+      context.minimalStep = response.data.minimalStep.amount
+      context.auctionId = response.data.auctionID
+    }).catch(error => {
+      console.log(`${context.$store.state.urls.auctionURL}database/${id}`)
+    })
+}
+export default getAuctionRequest
