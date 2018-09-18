6c2e1881e05d929611a8df797db177e41c35fca3
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-18 16:26:48 +0300

    imported function for calculatingDurationTime, setted progressbar line according to current_stage

--- src/components/StatusTimerLine.vue
+++ src/components/StatusTimerLine.vue
@@ -1,32 +1,51 @@
 <template>
   <transition>
-    <div v-if="state == 'active'" :style="progressStyles" class="progress">
+    <div :style="progressStyles" class="progress">
       <div class="progress-bar" />
     </div>
   </transition>
 </template>
 
 <script>
+import calculatingDurationTime from '../utils/calculatingDurationTime'
 export default {
   props: {
     remainedTimeOfRound: {
       type: Number,
       default: null
     },
-    durationOfRound: {
+    current_stage: {
       type: Number,
       default: null
     },
-    state: {
-      type: String,
+    stages: {
+      type: Array,
+      default: null
+    },
+    currentTime: {
+      type: Number,
       default: null
     },
   },
-  computed: { 
+  data(){
+    return{
+      i: 1,
+      progressStyles: 0
+    }
+  },
+  watch: { 
     progressStyles() {
-      return {
-        width: (100 - (this.remainedTimeOfRound / this.durationOfRound * 100)) + '%'
+      let calculate;
+      if(this.current_stage !== -1){
+        calculate = this.remainedTimeOfRound / calculatingDurationTime(this.stages[this.current_stage].start, this.stages[this.current_stage].planned_end )
       }
+      else if (this.current_stage === -1){
+        calculate = this.remainedTimeOfRound / ( this.remainedTimeOfRound + (this.i ++) )
+      }
+      else if(this.current_stage !== -1 && stages[this.current_stage].type === 'pause'){
+        calculate = this.remainedTimeOfRound / calculatingDurationTime(this.stages[this.current_stage].start, this.stages[this.current_stage + 1].start )
+      }
+      this.progressStyles = `width : ${(100 - (calculate * 100))}%`
     }
   }
 }
