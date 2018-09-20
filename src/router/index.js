054697e429cd438e37592cb32d60c40f66d1d141
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-20 14:26:32 +0300

    modified couchURL in config.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import MainView from '../views/MainView';
+import * as config from '../config.json';
+
 
 Vue.use(Router);
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: `/${config.auctionPrefix}/:id`,
       name: 'MainView',
       component: MainView,
+      props: true
     },
   ],
 });
