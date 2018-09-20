37f18cc0503b5b6965b3cb466d719cd3dec97b38
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2018-09-20 18:47:05 +0300

    Add function to get cookie by name

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,17 @@
-export default {}
+const getCookieByName =  (cname) => {
+  let name = cname + "="
+  let decodedCookie = decodeURIComponent(document.cookie)
+  let ca = decodedCookie.split(';')
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ''
+}
+
+export { getCookieByName }
