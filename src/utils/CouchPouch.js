0bf2e017367c3137dd41ce47eef507dff4f92db7
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-20 14:38:22 +0300

    made some changes

--- src/utils/CouchPouch.js
+++ src/utils/CouchPouch.js
@@ -2,15 +2,19 @@ import PouchDB from 'pouchdb'
 import infoPouch from '@/fakeInfo/infoPouchCouch'
 import config from '../config.json'
 
-const db = new PouchDB(config.pouchURL);
-let remoteDB = new PouchDB(config.couchURL)
-const fetch = (store) => {
-  store.commit('setInfoFromCouch', 'infoPouch');
-  db.allDocs({include_docs: true})
-    .then(r => r.rows)
-    .then(data => {
-      store.commit('setInfoFromCouch', 'infoPouch');
-    })
-    .catch(console.error)
-}
-export { db, remoteDB, fetch }
+// const db = new PouchDB(config.pouchURL);
+const pouchDB = new PouchDB('todo');
+// let remoteDB = new PouchDB(config.couchURL)
+let couchDB = new PouchDB(`${config.serverURL}database`)
+// const fetch = (store) => {
+//   console.log('pouch')
+//   db.allDocs({include_docs: true})
+//     .then(r => r.rows)
+//     .then(data => {
+//       console.log('data',data)
+//       // store.commit('setInfoFromCouch', data);
+//     })
+//     .catch(console.error)
+// }
+// export { db, remoteDB, fetch }
+export { pouchDB, couchDB }
