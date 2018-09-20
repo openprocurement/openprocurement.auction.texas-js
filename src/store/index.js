4141fc02e9d58e9689611f4fe55bf2da768d7776
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-20 14:35:31 +0300

    modified adding name of language to vue.i18n.

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import english from '../translations/english.js';
 import ukrainian from '../translations/ukrainian.js';
 import russian from '../translations/russian.js';
 import * as config from '../config.json';
-import { db, remoteDB, fetch } from '../utils/CouchPouch.js'
+import { pouchDB, couchDB } from '../utils/CouchPouch.js'
 
 Vue.use(Vuex);
 const debug = process.env.NODE_ENV !== 'production';
@@ -48,30 +48,27 @@ const store =  new Vuex.Store({
   }
 });
 // sync db - couch-pouch
-db.sync(remoteDB, {
-  live: true,
-  retry: true
-}).on('change', change => {
-  store.commit('setInfoFromCouch', 'infoFromCouch');
-  fetch(store)
-}).on('error', function (err) {
-  console.log('sync error', err)
-});
-// fetch initial
-// fetch(store) 
-
+// pouchDB.sync(couchDB, {
+//   live: true,
+//   retry: true
+// }).on('change', change => {
+//   // store.commit('setInfoFromCouch', change);
+//   console.log(change)
+// }).on('error', function (err) {
+//   console.log('sync error', err)
+// });
 
 window.onbeforeunload = function() {
-  return localStorage.setItem('language', store.state.i18n.locale);
+  localStorage.setItem('language', store.state.i18n.locale)
 };
 const language = localStorage.getItem('language');
 
 import vuexI18n from 'vuex-i18n'
 Vue.use(vuexI18n.plugin, store);
 
-Vue.i18n.add('English', english);
-Vue.i18n.add('Українська', ukrainian);
-Vue.i18n.add('Русский', russian);
+Vue.i18n.add('en', english);
+Vue.i18n.add('uk', ukrainian);
+Vue.i18n.add('ru', russian);
 Vue.i18n.fallback("English");
 Vue.i18n.set(language);
 
