b36172d7894244e5f52efcefb08ab08e53b3b480
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2018-09-24 18:48:20 +0300

    Add Notification component with vue-notifications

---
+++ src/components/Notification.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <notifications group="auth"/>
+    <notifications group="kickClient"/>
+    <notifications group="clientList"/>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{}
+  }
+};
+</script>
+<style>
+</style>
--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -5,10 +5,12 @@ import VueMoment from 'vue-moment';;
 import VueScrollTo from 'vue-scrollto';
 import ToggleSwitch from 'vuejs-toggle-switch';
 import VueSSE from 'vue-sse';
+import Notifications from 'vue-notification';
 
 Vue.use(VueSSE);
 Vue.use(ToggleSwitch);
 Vue.use(VueScrollTo);
 Vue.use(VueMoment);
+Vue.use(Notifications);
 Vue.use(Vuetify, {
 });
