b5c49a6feb4958c13dbdc8deff8b251137764002
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-09-26 18:39:24 +0300

    Changed all images for svg format

--- src/components/SpinnerLoader.vue
+++ src/components/SpinnerLoader.vue
@@ -1,16 +1,15 @@
 
 <template>
   <svg version="1.1" x="0px" y="0px"
-       width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-    <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
-      <animateTransform attributeType="xml"
-                        attributeName="transform"
-                        type="rotate"
-                        from="0 25 25"
-                        to="360 25 25"
-                        dur="2s"
-                        repeatCount="indefinite"/>
-    </path>
+       width="27" height="27" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+    <image x="1" y="1" width="50" height="50" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAYCAQAAAClvJ5NAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAAHdElNRQfiCRoMAB3P8FAeAAABPElEQVQ4y52TrUuDURSHn80x50BcsZksptmGfWVFRPwXTIJBLGoxGMS4v0C0mRcNgiD4BYpFEEUsfgRRhqggyGNwvnund/PV3y0H7u+599x7zklJQEUmgQeWQpsYWuOqXgX3SEdsgYT6Qoqcs5CQEbHko6orSRLLAHDLNQVgjl6mkTonwE2nW7DfIz81Gz67uZphn3tqzVxyBPPOm/0NkJQJv6mpDDNRfMpmG1c3U1F8jE2ttU2mEHNV0/xZGdajeKet6y3mOvjH8+OJpagEPQMMh6qPXa6qyz+ePuiFdUd+ljLrRuNHZlqArJeqPltuRXLWGsCWebFo1aqLIo76ouqrY3Gk4ruqNXsCzV/2SdV90/HEJnxzI+qw7/MyYt1d+763ZcmuDrM/ZL51xAAOOxbjLF79kO7ZBu7C9AfRVY2zfsYSNwAAAABJRU5ErkJggg=="/>
+    <animateTransform attributeType="xml"
+                      attributeName="transform"
+                      type="rotate"
+                      from="0"
+                      to="360"
+                      dur="2s"
+                      repeatCount="indefinite"/>
   </svg>
 </template>
 
@@ -19,10 +18,3 @@ export default {
 
 }
 </script>
-
-<style>
-svg path,
-svg rect{
-  fill: #ffffff;
-}
-</style>
