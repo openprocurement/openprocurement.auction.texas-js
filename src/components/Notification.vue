dbbf24fc77663461187b6fd16e9ae0328701e3b1
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2018-09-26 13:43:45 +0300

    Styles for position and custom template with button for kickClient

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,16 +1,53 @@
 <template>
-  <div>
-    <notifications group="auth"/>
-    <notifications group="kickClient"/>
-    <notifications group="clientList"/>
+  <div class="notifications-container">
+    <notifications :max="1" group="auth" class="custom-notifications"/>
+    <notifications group="kickClient" class="custom-notifications">
+      <template slot="body" slot-scope="props">
+        <div class="kick-client-template">
+          {{ props.item.text }}
+          <button class="kick-button" @click="kickNewClient(props.item.title) || props.close()">
+            Kick
+          </button>
+        </div>
+      </template>
+    </notifications>
+    <notifications group="clientList" class="custom-notifications"/>
   </div>
 </template>
 <script>
+import kickClient from "@/utils/kickClient";
+
 export default {
   data(){
-    return{}
+    return {}
+  },
+  methods: {
+    kickNewClient(clientId) {
+      kickClient(this.$store.state, clientId)
+    }
   }
 };
 </script>
 <style>
+.kick-client-template {
+  border: 1px solid black;
+  display: flex;
+}
+.custom-notifications {
+  display: flex;
+  position: static !important;
+  width: 100% !important;
+  flex-direction: column;
+}
+
+.notifications-container {
+  display: flex;
+  position: fixed;
+  flex-direction: column;
+  width: 100%;
+}
+
+.kick-button {
+  margin-left: 15px;
+}
 </style>
