6be8f8c93592a7c22dee3f25474e19c3555143f9
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-10-01 17:18:30 +0300

    Add styles for notifications messages

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,25 +1,42 @@
 <template>
   <div class="notifications-container">
-    <notifications :max="1" group="auth" class="custom-notifications"/>
-    <notifications group="kickClient" class="custom-notifications">
+    <notifications :max="1" :style="{color: '#0c5460', backgroundColor: '#d1ecf1', width: bgWidth, borderLeft: borderLeft}"
+                   group="auth" class="custom-notifications"/>
+    <notifications :style="{color: '#856404', backgroundColor: '#fff3cd', width: bgWidth, borderLeft: borderLeft}" 
+                   group="kickClient" class="custom-notifications">
       <template slot="body" slot-scope="props">
         <div class="kick-client-template">
-          {{ props.item.text }}
+          {{ $t(props.item.text) }}
           <button class="kick-button" @click="kickNewClient(props.item.title) || props.close()">
             Kick
           </button>
         </div>
       </template>
     </notifications>
-    <notifications group="clientList" class="custom-notifications"/>
+
+    <notifications group="utils" class="custom-notifications">
+      <template slot="body" slot-scope="props">
+        <div v-if="props.item.type === 'warning'" :style="{color: '#856404', backgroundColor: '#fff3cd', width: bgWidth, borderLeft: borderLeft, backgroundImage: bgImage}">
+          {{ $t(props.item.text) }}
+        </div>
+        <div v-else-if="props.item.type === 'error'" :style="{color: '#721c24', backgroundColor: '#f8d7da', width: bgWidth, borderLeft: borderLeft, backgroundImage: bgImage}">
+          {{ $t(props.item.text) }}
+        </div>
+      </template>
+    </notifications>
   </div>
 </template>
 <script>
 import kickClient from "@/utils/kickClient";
 
 export default {
-  data(){
-    return {}
+  data() {
+    return {
+      bgWidth: '100%',
+      bgHeight: '30px',
+      margin: '0',
+      borderLeft: 'none',
+    }
   },
   methods: {
     kickNewClient(clientId) {
@@ -30,8 +47,9 @@ export default {
 </script>
 <style>
 .kick-client-template {
-  border: 1px solid black;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .custom-notifications {
   display: flex;
@@ -49,5 +67,21 @@ export default {
 
 .kick-button {
   margin-left: 15px;
+  width: 100px;
+  height: 30px;
+  background-color: lightcoral;
 }
+
+.vue-notification {
+  background: none !important ;
+  color: inherit !important;
+  border-left: 1px solid black !important;
+  margin: 0 40px 5px !important;
+}
+
+.notification-wrapper:hover {
+  opacity: 0.8;
+  transition: 1s ease;
+}
+
 </style>
