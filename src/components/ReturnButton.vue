429eba9d4a3bf9d564ed4d4642785894c11a15ab
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-10-02 14:30:54 +0300

    Add arrow-button for return to tender

---
+++ src/components/ReturnButton.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div class="arrow-button">
+    <a :href="url">
+      <img class ="arrow-button"
+           src="/static_texas/images/left-arrow-button.svg" alt="left-arrow-button">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      url: this.$store.state.identification.returnURL
+    }
+  }
+}
+</script>
+
+<style>
+.arrow-button {
+    width: 25px;
+    height: 25px;
+    position: fixed;
+    top: 50%;
+}
+</style>
