8ddd0fd49ab1bda9a194a3c967e6bf5e34c88a66
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-10-04 17:26:39 +0300

    Remove script GTM from index.html, install vue-gtm package instead

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import ErrorAuctionId from '../views/ErrorAuctionId'
 import ErrorAuctionType from '../views/ErrorAuctionType'
 import * as config from '../config.json';
 
+import VueGtm from 'vue-gtm';
 
 Vue.use(Router);
 const router =  new Router({
@@ -28,4 +29,13 @@ const router =  new Router({
     },
   ],
 });
+
+Vue.use(VueGtm, {
+  id: 'GTM-P4Z7SRN', // Your GTM ID
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true, // Whether or not display console logs debugs (optional)
+  vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+  ignoredViews: ['route'] // If router, you can exclude some routes name (case insensitive) (optional)
+});
+
 export default router
