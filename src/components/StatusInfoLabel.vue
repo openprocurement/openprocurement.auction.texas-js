9c9de94263bb6107af5862f7db08928d3b6daf63
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-10-17 13:11:17 +0300

    Fix showing time in statusLabel

--- src/components/StatusInfoLabel.vue
+++ src/components/StatusInfoLabel.vue
@@ -16,7 +16,14 @@
       <svg v-else-if="state === 'completed'" class= "state_icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 22.12 17.01"><defs><clipPath id="d2e6e7f2-71df-4a16-823b-f86aae6e7c25" transform="translate(-4 -7)"><rect x="4" y="7" width="22.33" height="17.02" fill="none"/></clipPath></defs><g clip-path="url(#d2e6e7f2-71df-4a16-823b-f86aae6e7c25)"><path d="M4.25,16.36a.92.92,0,0,1-.25-.6.91.91,0,0,1,.25-.59L5.45,14a.82.82,0,0,1,1.19,0l.08.09,4.68,5a.41.41,0,0,0,.59,0L23.4,7.25h.09a.83.83,0,0,1,1.19,0l1.19,1.19a.82.82,0,0,1,0,1.19L12.25,23.76a.83.83,0,0,1-1.19,0L4.42,16.61Zm0,0" transform="translate(-4 -7)"/></g></svg>
       <svg v-else-if="state === 'active'" class= "state_icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 11.77 26.48"><defs><clipPath id="3e5076bd-8dec-47bf-b43b-b57e14aa31e4" transform="translate(-8 -2.39)"><rect x="8" y="2.39" width="11.74" height="26.49" fill="none"/></clipPath></defs><path d="M15.09,18.56" transform="translate(-8 -2.39)"/><path d="M14.07,18.54" transform="translate(-8 -2.39)"/><g clip-path="url(#3e5076bd-8dec-47bf-b43b-b57e14aa31e4)"><path fill="#fff" d="M9.18,28.87c-.28-.19,4.64-10.54,4.47-11S8.26,15.77,8,15,18.32,2.16,18.59,2.39,14,13,14.12,13.38s5.48,2.07,5.65,2.91S9.47,29.06,9.18,28.87Zm0,0" transform="translate(-8 -2.39)"/></g></svg>
       <div v-else-if="state == 'pendingOfRound'">
-        {{ remainedMinutesToStartRound }} хв
+        <div v-if="remainedMinutesToStartRound <= 1">
+          {{ remainedTimeOfRound }}
+          {{ $t("seconds") }}
+        </div>
+        <div v-else>
+          {{ remainedMinutesToStartRound }}
+          {{ $t("minutes") }}
+        </div>
       </div>
     </div>
   </div>
