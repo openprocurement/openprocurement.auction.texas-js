990fda748c2958fb804567092729ff42874cec36
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-10-18 18:37:13 +0300

    Add title and description with preseted language

--- src/utils/getRequest.js
+++ src/utils/getRequest.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import router from '../router'
 import parseCurrentStage from './parseCurrentStage'
 
+
 const fillAuctionData = (context, data) => {
   context.priceOffers = data.initial_bids
   context.startPrice = data.initial_value
@@ -10,9 +11,14 @@ const fillAuctionData = (context, data) => {
   context.endDate = data.endDate
   context.stages = data.stages
   context.companyName = data.procuringEntity.name
-  context.descriptionOfProducts = data.description
+  context.descriptionOfProducts.description_uk = data.description
+  context.descriptionOfProducts.description_ru = data.description_ru
+  context.descriptionOfProducts.description_en = data.description_en
   context.minimalStep = data.minimalStep.amount
   context.auctionId = data.auctionID
+  context.tenderTitles.tenderTitle_uk = data.title
+  context.tenderTitles.tenderTitle_ru = data.title_ru
+  context.tenderTitles.tenderTitle_en = data.title_en
   parseCurrentStage(data.stages, data.current_stage, context)
 }
 
