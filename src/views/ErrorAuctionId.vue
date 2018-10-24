4488e53237215ded21c62af963861dd99fbc0f1b
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-10-24 13:09:52 +0300

    Add styles for uncorrect link page and notFound page

--- src/views/ErrorAuctionId.vue
+++ src/views/ErrorAuctionId.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="not-found-container">
     {{ $t("Not Found") }}
   </div> 
 </template>
@@ -11,3 +11,26 @@ export default {
   }
 }
 </script>
+
+
+<style>
+  .not-found-container {
+    color: #a94442;
+    background-color: #f2dede;
+    border-color: #ebccd1;
+    padding: 15px;
+    margin-bottom: 20px;
+    border: 1px solid transparent;
+    border: transparent;
+    border-radius: 4px;
+    max-width: 767px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    min-height: 50px;
+    padding-left: 50px;
+}
+</style>
