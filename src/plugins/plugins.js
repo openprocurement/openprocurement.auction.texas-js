1f09d4199261a120967d9c150d991b3a40f22c53
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-11-12 14:43:24 +0200

    install new package, delete redundant from app

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -4,10 +4,8 @@ import 'vuetify/dist/vuetify.min.css';
 import VueMoment from 'vue-moment';;
 import VueScrollTo from 'vue-scrollto';
 import ToggleSwitch from 'vuejs-toggle-switch';
-import VueSSE from 'vue-sse';
 import Notifications from 'vue-notification';
 
-Vue.use(VueSSE);
 Vue.use(ToggleSwitch);
 Vue.use(VueScrollTo);
 Vue.use(VueMoment);
