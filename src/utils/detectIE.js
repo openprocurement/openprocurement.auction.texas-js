6bbe029e43cab5bb04b90119e80502788685432f
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2018-11-12 14:40:18 +0200

    Add function for detect browser IE and Edge

---
+++ src/utils/detectIE.js
@@ -0,0 +1,17 @@
+const  detectIE = () =>{
+  const ua = window.navigator.userAgent;
+  const msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+  const trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    const rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10)}
+  const edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10)}
+  return false
+}
+
+export default detectIE
