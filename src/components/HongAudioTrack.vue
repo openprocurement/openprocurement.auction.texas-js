198dabafa38df22f50914623e87007dadfdc6dfc
Vasyl Danyliv <vasyldanyliv@quintagroup.org>
2019-01-17 13:08:49 +0200

    Fix audio playing in devices that hame some restrictions fpr audio

--- src/components/HongAudioTrack.vue
+++ src/components/HongAudioTrack.vue
@@ -1,23 +1,11 @@
 <template>
-  <audio ref="player" autoplay>
+  <audio autoplay loop muted playsinline>
     <source src="/static_texas/media/GongHighFinal.wav">
     <source src="/static_texas/media/GongHighFinal.ogg">
     <source src="/static_texas/media/GongHighFinal.mp3">
     Your browser does not support the audio element.
   </audio>
 </template>
-
-<script>
-
-export default {
-  mounted: function () {
-    this.$watch('src', function () {
-      this.$refs.player.load();
-    });
-  },
-};
-
-</script>
 
 <style>
 </style>
